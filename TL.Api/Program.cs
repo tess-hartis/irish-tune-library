4665c852b6eb57985553be4b6621478094ae8e4f
tess-hartis <tess15hartis@gmail.com>
2022-01-15 22:04:27 -0800

    Add Progam file in API project

---
+++ TL.Api/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Newtonsoft.Json.Converters;
+using TinyHelpers.Json.Serialization;
+using TL.Data;
+using TL.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.Converters.Add(new DateOnlyConverter()))
+    .AddNewtonsoftJson(x => x.SerializerSettings.Converters.Add(new StringEnumConverter()));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ITuneRepository, TuneRepository>();
+builder.Services.AddDbContext<TuneLibraryContext>();
+builder.Services.AddSwaggerGenNewtonsoftSupport();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
