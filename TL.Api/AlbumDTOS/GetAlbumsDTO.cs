17beae42c94b9f1168c2f0f8cfb9cf608ec92220
tess-hartis <tess15hartis@gmail.com>
2022-01-17 19:05:39 -0800

    Add GetAlbumsDTO

---
+++ TL.Api/AlbumDTOS/GetAlbumsDTO.cs
@@ -0,0 +1,22 @@
+using TL.Api.TrackDTOS;
+using TL.Domain;
+
+namespace TL.Api.AlbumDTOS;
+
+public class GetAlbumsDTO
+{
+    private List<GetAlbumDTO> _albums { get; } = new List<GetAlbumDTO>();
+    public IEnumerable<GetAlbumDTO> AllAlbums => _albums;
+
+    public static GetAlbumsDTO GetAll(IEnumerable<Album> albums)
+    {
+        var dto = new GetAlbumsDTO();
+        foreach (var album in albums)
+        {
+            var albumDto = GetAlbumDTO.FromAlbum(album);
+            dto._albums.Add(albumDto);
+        }
+
+        return dto;
+    }
+}
