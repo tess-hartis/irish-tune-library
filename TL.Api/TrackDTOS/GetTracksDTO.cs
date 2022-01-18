2820b98fe16b69638992b39e82ff8b296e7db64a
tess-hartis <tess15hartis@gmail.com>
2022-01-17 18:10:59 -0800

    Add GetTracksDTO

---
+++ TL.Api/TrackDTOS/GetTracksDTO.cs
@@ -0,0 +1,21 @@
+using TL.Domain;
+
+namespace TL.Api.TrackDTOS;
+
+public class GetTracksDTO
+{
+    private   List<GetTrackDTO> _tracks { get; }
+    public IEnumerable<GetTrackDTO> Tracks = new List<GetTrackDTO>();
+
+    public static GetTracksDTO GetTracks(IEnumerable<Track> tracks)
+    {
+        var dto = new GetTracksDTO();
+        foreach (var track in tracks)
+        {
+            var trackDto = GetTrackDTO.FromTrack(track);
+            dto._tracks.Add(trackDto);
+        }
+
+        return dto;
+    }
+}
