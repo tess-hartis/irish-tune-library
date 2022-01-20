75492c1db6b306997a97061f8fee5286a4556b8b
tess-hartis <tess15hartis@gmail.com>
2022-01-19 19:17:53 -0800

    Add method to validate track numbers

--- TL.Common/IntValidator.cs
+++ TL.Common/IntValidator.cs
@@ -2,36 +2,8 @@ namespace TL.Common;
 
 public static class IntValidator
 {
-    public static bool IsValidInt(this int number)
-    {
-        //checks if all characters are numbers
-        foreach (char c in number.ToString())
-        {
-            if (!Char.IsNumber(c))
-            {
-                return false;
-            }
-        }
-        
-        //checks if number is zero
-        if (number < 1)
-        {
-            return false;
-        }
-
-        //checks if the number starts with zero
-        // var numberString = number.ToString();
-        // if (numberString.StartsWith("0"))
-        // {
-        //     return false;
-        // }
-
-        return true;
-    }
-
     public static bool IsValidYear(this int year)
     {
-        //checks if all characters are numbers
         foreach (char c in year.ToString())
         {
             if (!char.IsNumber(c))
@@ -40,9 +12,33 @@ public static class IntValidator
             }
         }
 
-        if (year > 3000)
+        switch (year)
         {
-            return false;
+            case > 2100:
+                return false;
+            case < 1800:
+                return false;
+        }
+
+        return true;
+    }
+
+    public static bool IsValidTrackNumber(this int track)
+    {
+        foreach (char c in track.ToString())
+        {
+            if (!char.IsNumber(c))
+            {
+                return false;
+            }
+        }
+        
+        switch (track)
+        {
+            case < 1:
+                return false;
+            case > 100:
+                return false;
         }
 
         return true;
