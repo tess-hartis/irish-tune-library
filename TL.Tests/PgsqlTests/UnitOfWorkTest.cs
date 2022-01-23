f9fdfa8d8dd0791b2f7fb09640c418bbe3683367
tess-hartis <tess15hartis@gmail.com>
2022-01-22 21:29:07 -0600

    Add method

--- TL.Tests/PgsqlTests/UnitOfWorkTest.cs
+++ TL.Tests/PgsqlTests/UnitOfWorkTest.cs
@@ -1,9 +1,31 @@
-using NUnit.Framework;
-
-namespace TL.Tests.PgsqlTests;
-
-[TestFixture]
-public class UnitOfWorkTest
-{
-    
-}
+// using NUnit.Framework;
+//
+// namespace TL.Tests.PgsqlTests;
+//
+// [TestFixture]
+// public class UnitOfWorkTest
+// {
+//     [Test]
+//     public async Task Can_Find_By_Tune_Featured()
+//     {
+//         //Arrange
+//         await using var context = new TuneLibraryContext();
+//         await context.Database.EnsureDeletedAsync();
+//         await context.Database.EnsureCreatedAsync();
+//         var repo = new TrackRepository(context);
+//         var track = Track.CreateTrack("Title", 1);
+//         await repo.AddTrack(track);
+//         var tune = Tune.CreateTune("Kesh", "Traditional", TuneTypeEnum.Jig, TuneKeyEnum.GMaj);
+//         context.Tunes.Add(tune);
+//         track.AddExistingTune(tune);
+//         await repo.SaveAsync();
+//         
+//         //Act
+//         const bool expected = true;
+//         var actual = track.TuneList.Contains(tune);
+//
+//         //Assert
+//         Assert.AreEqual(expected, actual);
+//
+//     }
+// }
