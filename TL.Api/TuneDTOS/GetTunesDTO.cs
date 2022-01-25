23d8fd5f8ee796e7dd5dc47fc906948697e09342
tess-hartis <tess15hartis@gmail.com>
2022-01-25 17:08:29 -0600

    Refactor Get Tunes DTO

--- TL.Api/TuneDTOS/GetTunesDTO.cs
+++ TL.Api/TuneDTOS/GetTunesDTO.cs
@@ -4,18 +4,19 @@ namespace TL.Api.TuneDTOs;
 
 public class GetTunesDTO
 {
-    private List<GetTuneDTO> _tunes { get; } = new List<GetTuneDTO>();
-    public IEnumerable<GetTuneDTO> AllTunes => _tunes;
+    // private List<GetTuneDTO> _tunes { get; } = new List<GetTuneDTO>();
+    // public IEnumerable<GetTuneDTO> AllTunes => _tunes;
 
-    public static GetTunesDTO GetAll(IEnumerable<Tune> tunes)
+    public List<GetTuneDTO> Tunes = new List<GetTuneDTO>();
+    public static List<GetTuneDTO> GetAll(IEnumerable<Tune> tunes)
     {
         var dto = new GetTunesDTO();
         foreach (var tune in tunes)
         {
             var tuneDto = GetTuneDTO.FromTune(tune);
-            dto._tunes.Add(tuneDto);
+            dto.Tunes.Add(tuneDto);
         }
-
-        return dto;
+    
+        return dto.Tunes;
     }
 }
