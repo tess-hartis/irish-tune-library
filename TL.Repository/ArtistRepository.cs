8515d3262a005095fcbe38dc024027f643b833e7
tess-hartis <tess15hartis@gmail.com>
2022-01-27 19:30:17 -0600

    Refactor Update Artist methods

--- TL.Repository/ArtistRepository.cs
+++ TL.Repository/ArtistRepository.cs
@@ -11,8 +11,7 @@ public interface IArtistRepository : IGenericRepository<Artist>
     new Task<Artist> FindAsync (int id);
     Task<IEnumerable<Artist>> GetAllArtists();
     Task<IEnumerable<Artist>> GetByExactName(string name);
-    Task UpdateName(int id, string name);
-
+    Task UpdateArtist(int id, string name);
 }
 
 public class ArtistRepository : GenericRepository<Artist>, IArtistRepository
@@ -25,14 +24,12 @@ public class ArtistRepository : GenericRepository<Artist>, IArtistRepository
     public async Task AddArtist(Artist artist)
     {
         await AddAsync(artist);
-       
     }
 
     public async Task DeleteArtist(int id)
     {
         var artist = await FindAsync(id);
         await DeleteAsync(artist);
-        
     }
 
     public override async Task<Artist> FindAsync (int id)
@@ -56,7 +53,7 @@ public class ArtistRepository : GenericRepository<Artist>, IArtistRepository
         return await GetByWhere(artist => artist.Name == name).ToListAsync();
     }
 
-    public async Task UpdateName(int id, string name)
+    public async Task UpdateArtist(int id, string name)
     {
         var artist = await FindAsync(id);
         artist.UpdateName(name);
