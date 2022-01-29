7f1690b812986bdec99b9918b5e0697999a4d6f8
tess-hartis <tess15hartis@gmail.com>
2022-01-29 09:27:24 -0600

    Remove comments

--- TL.Repository/GenericRepository.cs
+++ TL.Repository/GenericRepository.cs
@@ -34,10 +34,6 @@ public abstract class GenericRepository<T>
     public IQueryable<T> GetByWhere(Expression<Func<T, bool>> predicate)
     {
         var result = Context.Set<T>().Where(predicate).AsNoTracking();
-        // if (!result.IsAny())
-        // {
-        //     throw new Exception();
-        // }
 
         return result;
     }
