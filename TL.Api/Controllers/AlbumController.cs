876ed33ea7277d8bcf0564de36a5302adb3f6bb0
tess-hartis <tess15hartis@gmail.com>
2022-01-29 09:31:16 -0600

    Refactor controller

--- TL.Api/Controllers/AlbumController.cs
+++ TL.Api/Controllers/AlbumController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using TL.Api.AlbumDTOs;
+using TL.Api.ArtistDTOs;
 using TL.Api.TrackDTOs;
 using TL.Repository;
 
@@ -10,37 +11,31 @@ public class AlbumController : Controller
 {
   private readonly IAlbumRepository _albumRepository;
   private readonly IAlbumArtistService _albumArtistService;
+  private readonly IAlbumTrackService _albumTrackService;
 
-  public AlbumController(IAlbumRepository albumRepository, IAlbumArtistService albumArtistService)
+  public AlbumController(IAlbumRepository albumRepository, IAlbumArtistService albumArtistService,
+    IAlbumTrackService albumTrackService)
   {
     _albumRepository = albumRepository;
     _albumArtistService = albumArtistService;
+    _albumTrackService = albumTrackService;
   }
 
   [HttpGet("{id}")]
   public async Task<ActionResult<GetAlbumDTO>> FindAlbum(int id)
   {
-    var album = await _albumRepository.FindAsync(id);
+    var album = await _albumRepository.FindAlbum(id);
     var returned = GetAlbumDTO.FromAlbum(album);
     return Ok(returned);
   }
-
-  [HttpGet("{title}")]
-  public async Task<ActionResult<IEnumerable<GetTracksDTO>>> FindByTitle(string title)
-  {
-    var albums = await _albumRepository.FindByExactTitle(title);
-    var returned = GetAlbumsDTO.GetAll(albums);
-    return Ok(returned);
-  }
-
+  
   [HttpGet]
-  public async Task<ActionResult<IEnumerable<GetAlbumsDTO>>> FindAll()
+  public async Task<ActionResult<IEnumerable<GetAlbumsDTO>>> FindAllAlbums()
   {
     var albums = await _albumRepository.GetAllAlbums();
     var returned = GetAlbumsDTO.GetAll(albums);
     return Ok(returned);
   }
-  
   
   [HttpPost]
   public async Task<ActionResult> AddAlbum([FromBody] PostAlbumDTO dto)
@@ -55,16 +50,51 @@ public class AlbumController : Controller
   {
     var album = await _albumRepository.FindAsync(id);
     var updated = PutAlbumDTO.UpdatedAlbum(album, dto);
-    await _albumRepository.UpdateAsync(updated);
+    await _albumRepository.UpdateAlbum(updated.Id, dto.Title, dto.Year);
     return Ok();
   }
   
   [HttpDelete("{id}")]
   public async Task<ActionResult> DeleteAlbum(int id)
   {
-    var album = _albumRepository.FindAsync(id);
+    var album = await _albumRepository.FindAsync(id);
     await _albumRepository.DeleteAlbum(album.Id);
     return Ok();
 
   }
+
+  [HttpPost("{albumId}/artist/{artistId}")]
+  public async Task<ActionResult> AddExistingArtistToAlbum(int albumId, int artistId)
+  {
+    await _albumArtistService.AddExistingArtistToAlbum(albumId, artistId);
+    return Ok();
+  }
+
+  [HttpPost("{albumId}/artist")]
+  public async Task<ActionResult> AddNewArtistToAlbum(int albumId, [FromBody] PostArtistDTO dto)
+  {
+    await _albumArtistService.AddNewArtistToAlbum(albumId, dto.Name);
+    return Ok();
+  }
+
+  // [HttpDelete("{albumId}/removeArtist")]
+  // public async Task<ActionResult> RemoveArtistFromAlbum(int albumId, [FromBody] AddRemoveArtistDTO dto)
+  // {
+  //   await _albumArtistService.RemoveArtistFromAlbum(albumId, dto.Id);
+  //   return Ok();
+  // }
+
+  [HttpPost("{albumId}/track")]
+  public async Task<ActionResult> AddTrackToAlbum(int albumId, [FromBody] PostTrackDTO dto)
+  {
+    await _albumTrackService.AddNewTrackToAlbum(albumId, dto.Title, dto.TrackNumber);
+    return Ok();
+  }
+
+  // [HttpPut("{albumId}/removeTrack")]
+  // public async Task<ActionResult> RemoveTrackFromAlbum(int albumId, [FromBody] RemoveTrackDTO dto)
+  // {
+  //   await _albumTrackService.RemoveTrackFromAlbum(albumId, dto.Id);
+  //   return Ok();
+  // }
 }
