d87d794e8e1f39cbd395e7e8aab14f2bb81cbc47
tess-hartis <tess15hartis@gmail.com>
2022-01-30 21:09:15 -0600

    Create ExceptionHandlingMiddleware.cs

---
+++ TL.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using TL.Domain.Exceptions;
+
+namespace TL.Api.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _requestDelegate;
+    public ExceptionHandlingMiddleware(RequestDelegate requestDelegate)
+    {
+        _requestDelegate = requestDelegate;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+
+        try
+        {
+            await _requestDelegate(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(context, ex);
+        }
+    }
+    
+    private static Task HandleException(HttpContext context, Exception ex)
+    {
+        var statusCode = (int) HttpStatusCode.InternalServerError;
+        switch (ex)
+        {
+            case InvalidEntityException:
+                statusCode = (int) HttpStatusCode.UnprocessableEntity;
+                break;
+            
+                
+        }
+        
+        var errorMessage = JsonConvert.SerializeObject(new {ex.Message});
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+
+        return context.Response.WriteAsync(errorMessage);
+    }
+}
