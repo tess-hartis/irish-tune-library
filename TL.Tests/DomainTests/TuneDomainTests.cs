41968a93e0b7aa1b5912e6f96b01dab4eb55f006
tess-hartis <tess15hartis@gmail.com>
2022-01-31 17:59:37 -0600

    Remove tests

--- TL.Tests/DomainTests/TuneDomainTests.cs
+++ TL.Tests/DomainTests/TuneDomainTests.cs
@@ -32,44 +32,8 @@ public class TuneDomainTests
             Tune.CreateTune("Title", "", TuneTypeEnum.Air, TuneKeyEnum.Ador));
     }
 
-    [Test]
-    public void Can_Update_Properties_Using_Access_Methods()
-    {
-        //Arrange
-        var tune = Tune.CreateTune("Title", "Composer", TuneTypeEnum.Air, TuneKeyEnum.Ador);
-        
-        //Act
-        tune.UpdateTitle("New Title");
-        tune.UpdateComposer("New Composer");
-        tune.UpdateType(TuneTypeEnum.Fling);
-        tune.UpdateKey(TuneKeyEnum.Amin);
-        
-        //Assert
-        Assert.AreEqual("New Title", tune.Title);
-        Assert.AreEqual("New Composer", tune.Composer);
-        Assert.AreEqual(TuneTypeEnum.Fling, tune.TuneType);
-        Assert.AreEqual(TuneKeyEnum.Amin, tune.TuneKey);
-    }
-
-    [Test]
-    public void Cannot_Update_Title_With_Invalid_Input()
-    {
-        //Arrange
-        var tune = Tune.CreateTune("Title", "Composer", TuneTypeEnum.Air, TuneKeyEnum.Ador);
-        
-        //Assert
-        Assert.Throws<FormatException>(() => tune.UpdateTitle(""));
-    }
     
-    [Test]
-    public void Cannot_Update_Composer_With_Invalid_Input()
-    {
-        //Arrange
-        var tune = Tune.CreateTune("Title", "Composer", TuneTypeEnum.Air, TuneKeyEnum.Ador);
-        
-        //Assert
-        Assert.Throws<FormatException>(() => tune.UpdateComposer(""));
-    }
+
     
     
     
