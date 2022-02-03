7857916c15751a92b053aedf0257d9583c70e759
tess-hartis <tess15hartis@gmail.com>
2022-02-02 21:29:16 -0600

    Remove endpoint

--- TL.Api/Controllers/TrackController.cs
+++ TL.Api/Controllers/TrackController.cs
@@ -72,11 +72,5 @@ public class TrackController : Controller
         await _tuneTrackService.AddExistingTuneToTrack(trackId, tuneId);
         return Ok();
     }
-
-    // [HttpPost("{id}/tune")]
-    // public async Task<ActionResult> AddNewTuneToTrack(int id, [FromBody] PostTuneDTO dto)
-    // {
-    //     await _tuneTrackService.AddNewTuneToTrack(id, dto.Title, dto.Composer, dto.Type, dto.Key);
-    //     return Ok();
-    // }
+    
 }
