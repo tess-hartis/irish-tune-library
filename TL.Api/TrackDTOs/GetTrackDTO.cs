6ca6ee94ec00996a8934ceeb1128cfe8605539be
tess-hartis <tess15hartis@gmail.com>
2022-02-05 12:52:29 -0600

    Remove album property from TrackDto

--- TL.Api/TrackDTOs/GetTrackDTO.cs
+++ TL.Api/TrackDTOs/GetTrackDTO.cs
@@ -6,7 +6,6 @@ public class GetTrackDTO
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    // public Album Album { get; set; }
     public int TrackNumber { get; set; }
     
     public static GetTrackDTO FromTrack(Track track)
@@ -15,8 +14,8 @@ public class GetTrackDTO
         {
             Id = track.Id,
             Title = track.Title,
-            // Album = track.Album,
-            TrackNumber = track.TrackNumber
+            TrackNumber = track.TrackNumber,
+            
         };
     }
 }
