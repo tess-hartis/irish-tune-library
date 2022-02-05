2d9bac72709b0306ae01a6530eee717c742b57b9
tess-hartis <tess15hartis@gmail.com>
2022-02-05 12:42:33 -0600

    Add Dto for getting TrackTune

---
+++ TL.Api/TrackDTOs/GetTrackTuneDTO.cs
@@ -0,0 +1,20 @@
+using TL.Domain;
+
+namespace TL.Api.TrackDTOs;
+
+public class GetTrackTuneDTO
+{
+    public int TuneId { get; set; }
+    public string Title { get; set; }
+    public int Order { get; set; }
+
+    public static GetTrackTuneDTO FromTrackTune(TrackTune trackTune)
+    {
+        return new GetTrackTuneDTO
+        {
+            TuneId = trackTune.TuneId,
+            Title = trackTune.Title,
+            Order = trackTune.Order
+        };
+    }
+}
