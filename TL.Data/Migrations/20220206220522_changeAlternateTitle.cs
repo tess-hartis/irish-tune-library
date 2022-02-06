636528be63c2adb23adf614f773c03a3cca6460c
tess-hartis <tess15hartis@gmail.com>
2022-02-06 16:36:06 -0600

    Migration files

---
+++ TL.Data/Migrations/20220206220522_changeAlternateTitle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TL.Data.Migrations
+{
+    public partial class changeAlternateTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
