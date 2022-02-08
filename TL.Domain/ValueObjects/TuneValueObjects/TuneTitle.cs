b5e81e746feaf7a703b8ef76fbbae40fcea15d18
tess-hartis <tess15hartis@gmail.com>
2022-02-08 16:59:37 -0600

    Add comments for future validation methods

--- TL.Domain/ValueObjects/TuneValueObjects/TuneTitle.cs
+++ TL.Domain/ValueObjects/TuneValueObjects/TuneTitle.cs
@@ -1,4 +1,6 @@
-using Microsoft.VisualBasic.CompilerServices;
+using LanguageExt;
+using static LanguageExt.Prelude;
+using LanguageExt.Common;
 
 namespace TL.Domain.ValueObjects.TuneValueObjects;
 
@@ -19,3 +21,40 @@ public record TuneTitle
         return new TuneTitle(value);
     }
 }
+
+// public static Either<List<string>, TuneTitle> Create(string value)
+    // {
+    //     var errors = new List<string>();
+    //     
+    //     if(string.IsNullOrWhiteSpace(value))
+    //         errors.Add("cannot exceed 100 char");
+    //
+    //     if (errors.Any())
+    //         return Left<List<string>, TuneTitle>(errors);
+    //
+    //     return Right<List<string>, TuneTitle>(new TuneTitle(value));
+    //     
+    // }
+
+    // public static Validation<Error, TuneTitle> Validate(string title)
+    // {
+    //     var titleV = MaxStrLength(100)(title);
+    //     return (titleV).Apply((t) => new TuneTitle(title));
+    // }
+    //
+    // public static Func<string, Validation<Error, string>> MaxStrLength(int max) =>
+    //     s => s.Length <= max
+    //         ? Success<Error, string>(s)
+    //         : Fail<Error, string>(Error.New("exceeds max length"));
+
+//     public static Validation<Error, string> NonEmpty(string str) =>
+//         String.IsNullOrEmpty(str)
+//             ? Validation<Error, String>.Fail(Seq<Error>().Add(Error.New("Non empty string is required")))
+//             : Validation<Error, String>.Success(str);
+//     
+// }
+//
+// public class Error : NewType<Error, string>
+// {
+//     public Error(string e) : base(e) { }
+// }
