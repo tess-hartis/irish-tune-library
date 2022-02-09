829075405d6e56c76fcf20dcf74bba752a8e061a
tess-hartis <tess15hartis@gmail.com>
2022-02-08 23:07:24 -0600

    Remove validation

--- TL.Domain/ValueObjects/TrackValueObjects/TrackNumber.cs
+++ TL.Domain/ValueObjects/TrackValueObjects/TrackNumber.cs
@@ -2,7 +2,7 @@ namespace TL.Domain.ValueObjects.TrackValueObjects;
 
 public record TrackNumber
 {
-    public readonly int Value;
+    public int Value;
 
     private TrackNumber(int value)
     {
@@ -11,8 +11,6 @@ public record TrackNumber
 
     public static TrackNumber Create(int value)
     {
-        if (value < 1 || value > 50)
-            throw new Exception();
 
         return new TrackNumber(value);
     }
