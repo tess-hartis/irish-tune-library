e801e75964953cea72749f5a118bf24ad093ce1e
tess-hartis <tess15hartis@gmail.com>
2022-02-09 16:33:03 -0600

    Add test for AlbumTrackService

---
+++ TL.Tests/PgsqlTests/AlbumTrackServiceTest.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TL.Data;
+using TL.Domain;
+using TL.Domain.ValueObjects.AlbumValueObjects;
+using TL.Domain.ValueObjects.TrackValueObjects;
+using TL.Repository;
+
+namespace TL.Tests.PgsqlTests;
+
+[TestFixture]
+public class AlbumTrackServiceTest
+{
+    [Test]
+    public async Task Can_Add_Track_To_Album_And_Set_TrackNumber()
+    {
+        //Arrange
+         await using var context = new TuneLibraryContext();
+         await context.Database.EnsureDeletedAsync();
+         await context.Database.EnsureCreatedAsync();
+         var albumRepo = new AlbumRepository(context);
+         var trackRepo = new TrackRepository(context);
+         var albumTrackRepo = new AlbumTrackService(context, albumRepo, trackRepo);
+         var title = AlbumTitle.Create("Making Time");
+         var year = AlbumYear.Create(2000);
+         var album = Album.CreateAlbum(title, year);
+         await albumRepo.AddAsync(album);
+         
+         //Act
+         var trackTitle = TrackTitle.Create("new Track");
+         var trackNumber = TrackNumber.Create(3);
+         await albumTrackRepo.AddNewTrackToAlbum(album.Id, trackTitle, trackNumber);
+         
+         //Assert
+         var foundTrack = await trackRepo.FindAsync(1);
+         var expected = 3;
+         var actual = foundTrack.TrackNumber.Value;
+         
+         Assert.AreEqual(expected, actual);
+
+    }
+
+}
