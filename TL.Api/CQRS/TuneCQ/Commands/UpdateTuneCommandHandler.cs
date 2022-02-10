2348fd70a44fa1c0b20f83740d9042f04502562c
tess-hartis <tess15hartis@gmail.com>
2022-02-09 22:41:28 -0600

    Add command handler for PutTune endpoint

---
+++ TL.Api/CQRS/TuneCQ/Commands/UpdateTuneCommandHandler.cs
@@ -0,0 +1,45 @@
+using CSharpFunctionalExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TL.Domain;
+using TL.Domain.ValueObjects.TuneValueObjects;
+using TL.Repository;
+
+namespace TL.Api.CQRS.TuneCQ.Commands;
+
+public class UpdateTuneCommand : IRequest<Tune>
+{
+    public int Id { get; }
+    public TuneTitle Title { get; }
+    public TuneComposer Composer { get; }
+    public string Type { get; }
+    public string Key { get; }
+
+    public UpdateTuneCommand(int id, TuneTitle title, TuneComposer composer, string type, string key)
+    {
+        Id = id;
+        Title = title;
+        Composer = composer;
+        Type = type;
+        Key = key;
+    }
+}
+
+public class UpdateTuneCommandHandler : IRequestHandler<UpdateTuneCommand, Tune>
+{
+    private readonly ITuneRepository _tuneRepository;
+
+    public UpdateTuneCommandHandler(ITuneRepository tuneRepository)
+    {
+        _tuneRepository = tuneRepository;
+    }
+
+    public async Task<Tune> Handle(UpdateTuneCommand command, CancellationToken cancellationToken)
+    {
+        var tune = await _tuneRepository.FindAsync(command.Id);
+        await _tuneRepository.UpdateTune
+            (tune.Id, command.Title, command.Composer, command.Type, command.Key);
+        return tune;
+
+    }
+}
