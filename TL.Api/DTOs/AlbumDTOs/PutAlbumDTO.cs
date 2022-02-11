7a93c34cf930481a697dc68ff35b47e346aeb732
tess-hartis <tess15hartis@gmail.com>
2022-02-11 16:07:00 -0600

    Remove static method from PutAlbumDTO.cs

--- TL.Api/DTOs/AlbumDTOs/PutAlbumDTO.cs
+++ TL.Api/DTOs/AlbumDTOs/PutAlbumDTO.cs
@@ -1,4 +1,3 @@
-using TL.Domain;
 using TL.Domain.ValueObjects.AlbumValueObjects;
 
 namespace TL.Api.DTOs.AlbumDTOs;
@@ -7,12 +6,4 @@ public class PutAlbumDTO
 {
     public string Title { get; set; }
     public int Year { get; set; }
-
-    public static Album UpdatedAlbum(Album album, PutAlbumDTO dto)
-    {
-        var title = AlbumTitle.Create(dto.Title);
-        var year = AlbumYear.Create(dto.Year);
-        album.Update(title, year);
-        return album;
-    }
 }
