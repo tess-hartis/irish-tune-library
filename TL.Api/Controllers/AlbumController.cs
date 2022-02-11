1bc4b00d888114a9ca240090ae2f97cf46907219
tess-hartis <tess15hartis@gmail.com>
2022-02-11 16:06:05 -0600

    Edit using declarations

--- TL.Api/Controllers/AlbumController.cs
+++ TL.Api/Controllers/AlbumController.cs
@@ -1,7 +1,9 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TL.Api.CQRS.Album.Queries;
+using TL.Api.CQRS.AlbumCQ.Queries;
+using TL.Api.CQRS.AlbumCQ.Commands;
+using TL.Api.CQRS.Track.Commands;
 using TL.Api.DTOs.AlbumDTOs;
 using TL.Api.DTOs.ArtistDTOs;
 using TL.Api.DTOs.TrackDTOs;
@@ -15,17 +17,10 @@ namespace TL.Api.Controllers;
 [Route("api/[controller]")]
 public class AlbumController : Controller
 {
-  private readonly IAlbumRepository _albumRepository;
-  private readonly IAlbumArtistService _albumArtistService;
-  private readonly IAlbumTrackService _albumTrackService;
   private readonly IMediator _mediator;
 
-  public AlbumController(IAlbumRepository albumRepository, IAlbumArtistService albumArtistService,
-    IAlbumTrackService albumTrackService, IMediator mediator)
+  public AlbumController(IMediator mediator)
   {
-    _albumRepository = albumRepository;
-    _albumArtistService = albumArtistService;
-    _albumTrackService = albumTrackService;
     _mediator = mediator;
   }
 
@@ -49,9 +44,11 @@ public class AlbumController : Controller
   public async Task<ActionResult> AddAlbum([FromBody] PostAlbumDTO dto)
   {
     var album = PostAlbumDTO.ToAlbum(dto);
-    await _albumRepository.AddAsync(album);
-    var returned = GetAlbumDTO.FromAlbum(album);
-    return CreatedAtAction(nameof(FindAlbum), new {id = album.Id}, returned);
+    var command = new CreateAlbumCommand(album.Title, album.Year);
+    var result = await _mediator.Send(command);
+    return Ok(result);
+    
+    // return CreatedAtAction(nameof(FindAlbum), new {id = album.Id}, returned);
   }
   
   [HttpPut("{id}")]
@@ -59,53 +56,61 @@ public class AlbumController : Controller
   {
     var title = AlbumTitle.Create(dto.Title);
     var year = AlbumYear.Create(dto.Year);
-    await _albumRepository.UpdateAlbum(id, title, year);
-    return Ok($"Album with ID '{id}' was updated");
+    var command = new UpdateAlbumCommand(id, title, year);
+    var result = await _mediator.Send(command);
+    return Ok(result);
   }
   
   [HttpDelete("{id}")]
   public async Task<ActionResult> DeleteAlbum(int id)
   {
-    await _albumRepository.DeleteAsync(id);
+    var command = new DeleteAlbumCommand(id);
+    await _mediator.Send(command);
     return Ok($"Album with ID '{id}' was deleted");
   }
 
   [HttpPost("{albumId}/artist/{artistId}")]
   public async Task<ActionResult> AddExistingArtistToAlbum(int albumId, int artistId)
   {
-    await _albumArtistService.AddExistingArtistToAlbum(albumId, artistId);
-    return Ok($"Artist with ID '{artistId} was added to album with ID '{albumId}'");
+    var command = new AddExistingArtistToAlbumCommand(albumId, artistId);
+    var result = await _mediator.Send(command);
+    return Ok(result);
+    
+    //need to edit DTO to include album artists
   }
 
   [HttpPost("{albumId}/artist")]
   public async Task<ActionResult> AddNewArtistToAlbum(int albumId, [FromBody] PostArtistDTO dto)
   {
-    var name = ArtistName.Create(dto.Name);
-    await _albumArtistService.AddNewArtistToAlbum(albumId, name);
-    return Ok($"New artist '{dto.Name}' was added to album with ID '{albumId}'");
+    var artist = PostArtistDTO.ToArtist(dto);
+    var command = new AddNewArtistToAlbumCommand(albumId, artist.Name);
+    var result = await _mediator.Send(command);
+    return Ok(result);
   }
 
   [HttpDelete("{albumId}/artist/{artistId}")]
   public async Task<ActionResult> RemoveArtistFromAlbum(int albumId, int artistId)
   {
-    await _albumArtistService.RemoveArtistFromAlbum(albumId, artistId);
-    return Ok($"Artist with ID '{artistId}' was removed from album with ID '{albumId}'");
+    var command = new RemoveArtistFromAlbumCommand(albumId, artistId);
+    var result = await _mediator.Send(command);
+    return Ok(result);
   }
 
   [HttpPost("{albumId}/track")]
   public async Task<ActionResult> AddTrackToAlbum(int albumId, [FromBody] PostTrackDTO dto)
   {
     var track = PostTrackDTO.ToTrack(dto);
-    await _albumTrackService.AddNewTrackToAlbum(albumId, track.Title, track.TrackNumber);
-    return Ok($"New track with TRACK NUM {track.TrackNumber.Value} was successfully added");
+    var command = new AddTrackToAlbumCommand(albumId, track.Title, track.TrackNumber);
+    var result = await _mediator.Send(command);
+    return Ok($"Track added to album: {result}");
   }
 
-  [HttpDelete("{albumId}/track/{trackId}")]
-  public async Task<ActionResult> RemoveTrackFromAlbum(int albumId, int trackId)
-  {
-    await _albumTrackService.RemoveTrackFromAlbum(albumId, trackId);
-    return Ok($"Track with ID '{trackId}' was removed from album with ID '{albumId}'");
-  }
+  // [HttpDelete("{albumId}/track/{trackId}")]
+  // public async Task<ActionResult> RemoveTrackFromAlbum(int albumId, int trackId)
+  // {
+  //   await _albumTrackService.RemoveTrackFromAlbum(albumId, trackId);
+  //   return Ok($"Track with ID '{trackId}' was removed from album with ID '{albumId}'");
+  // }
 
   [HttpGet("{albumId}/tracks")]
   public async Task<ActionResult<IEnumerable<GetTrackDTO>>> GetAlbumTracks(int albumId)
