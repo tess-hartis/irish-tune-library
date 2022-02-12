0535e274912dd6e63d62b67db73126f3f97b3c7f
tess-hartis <tess15hartis@gmail.com>
2022-02-11 22:08:03 -0600

    Update GetTrackDTO to include albumId and list of TrackTuneDTOs

--- TL.Api/DTOs/TrackDTOs/GetTrackDTO.cs
+++ TL.Api/DTOs/TrackDTOs/GetTrackDTO.cs
@@ -4,18 +4,24 @@ namespace TL.Api.DTOs.TrackDTOs;
 
 public class GetTrackDTO
 {
-    public int Id { get; set; }
+    public int TrackId { get; set; }
+    public int AlbumId { get; set; }
     public string Title { get; set; }
     public int TrackNumber { get; set; }
-    
+    public IEnumerable<GetTrackTuneDTO> Tunes { get; set; }= new List<GetTrackTuneDTO>();
+
     public static GetTrackDTO FromTrack(Track track)
     {
+        var tunes = track.TrackTunes.Select(GetTrackTuneDTO.FromTrackTune);
+        
         return new GetTrackDTO
         {
-            Id = track.Id,
+            TrackId = track.Id,
+            AlbumId = track.AlbumId,
             Title = track.Title.Value,
             TrackNumber = track.TrackNumber.Value,
-            
+            Tunes = tunes
+
         };
     }
 }
