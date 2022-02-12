ef71ea5cf1c24d736430cd5568f4bca4c4720c19
tess-hartis <tess15hartis@gmail.com>
2022-02-12 09:11:34 -0600

    Add method to get all tracks and include tunelist

--- TL.Repository/TrackRepository.cs
+++ TL.Repository/TrackRepository.cs
@@ -11,6 +11,7 @@ public interface ITrackRepository : IGenericRepository<Track>
 {
     new Task<Track> FindAsync(int id);
     Task UpdateTrack(int id, TrackTitle title, TrackNumber trackNumber);
+    Task<IEnumerable<Track>> GetAll();
 }
 
 public class TrackRepository : GenericRepository<Track>, ITrackRepository
@@ -38,5 +39,12 @@ public class TrackRepository : GenericRepository<Track>, ITrackRepository
         track.Update(title, trackNumber);
         await SaveAsync();
     }
-    
+
+    public async Task<IEnumerable<Track>> GetAll()
+    {
+        return await Context.Tracks.Include(t => t.TrackTunes).ToListAsync();
+    }
+
+
+
 }
