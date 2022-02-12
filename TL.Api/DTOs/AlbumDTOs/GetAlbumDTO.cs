7e8068bfb88dc18d9180c8093777689ac44ebe2c
tess-hartis <tess15hartis@gmail.com>
2022-02-12 09:12:59 -0600

    Update GetAlbumDTO to include list of artists on album

--- TL.Api/DTOs/AlbumDTOs/GetAlbumDTO.cs
+++ TL.Api/DTOs/AlbumDTOs/GetAlbumDTO.cs
@@ -7,14 +7,18 @@ public class GetAlbumDTO
     public int Id { get; set; }
     public string Title { get; set; }
     public int Year { get; set; }
+    public IEnumerable<string> Artists { get; set; }
 
     public static GetAlbumDTO FromAlbum(Album album)
     {
+        var artistNames = album.Artists.Select(x => x.Name.Value);
+        
         return new GetAlbumDTO
         {
             Id = album.Id,
             Title = album.Title.Value,
-            Year = album.Year.Value
+            Year = album.Year.Value,
+            Artists = artistNames
         };
     }
 }
