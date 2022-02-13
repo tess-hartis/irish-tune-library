e9fb876711f51dc9f08748bd3ac1ce117069cabd
tess-hartis <tess15hartis@gmail.com>
2022-02-12 18:04:48 -0600

    Delete unused DTOs

--- TL.Api/DTOs/AlbumDTOs/PutAlbumDTO.cs
+++
@@ -1,9 +0,0 @@
-using TL.Domain.ValueObjects.AlbumValueObjects;
-
-namespace TL.Api.DTOs.AlbumDTOs;
-
-public class PutAlbumDTO
-{
-    public string Title { get; set; }
-    public int Year { get; set; }
-}
