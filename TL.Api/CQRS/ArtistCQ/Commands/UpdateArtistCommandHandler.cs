765b3f7746dc9d8054fc9ac64bc322c8097145c7
tess-hartis <tess15hartis@gmail.com>
2022-02-12 18:04:37 -0600

    Update command handler to unwrap validation from artist value object validations

--- TL.Api/CQRS/ArtistCQ/Commands/UpdateArtistCommandHandler.cs
+++ TL.Api/CQRS/ArtistCQ/Commands/UpdateArtistCommandHandler.cs
@@ -1,3 +1,5 @@
+using LanguageExt;
+using LanguageExt.Common;
 using MediatR;
 using TL.Domain;
 using TL.Domain.ValueObjects.ArtistValueObjects;
@@ -5,18 +7,19 @@ using TL.Repository;
 
 namespace TL.Api.CQRS.ArtistCQ.Commands;
 
-public class UpdateArtistCommand : IRequest<Artist>
+public class UpdateArtistCommand : IRequest<Validation<Error, Artist>>
 {
-    public int Id { get; }
-    public ArtistName Name { get; }
+    public int Id { get; set; }
+    public string Name { get; }
 
-    public UpdateArtistCommand(int id, ArtistName name)
+    public UpdateArtistCommand(int id, string name)
     {
         Id = id;
         Name = name;
     }
 }
-public class UpdateArtistCommandHandler : IRequestHandler<UpdateArtistCommand, Artist>
+public class UpdateArtistCommandHandler : 
+    IRequestHandler<UpdateArtistCommand, Validation<Error, Artist>>
 {
     private readonly IArtistRepository _artistRepository;
 
@@ -25,9 +28,17 @@ public class UpdateArtistCommandHandler : IRequestHandler<UpdateArtistCommand, A
         _artistRepository = artistRepository;
     }
 
-    public async Task<Artist> Handle(UpdateArtistCommand command, CancellationToken cancellationToken)
+    public async Task<Validation<Error, Artist>> Handle
+        (UpdateArtistCommand command, CancellationToken cancellationToken)
     {
-        var artist = await _artistRepository.UpdateArtist(command.Id, command.Name);
-        return artist;
+        var artist = await _artistRepository.FindAsync(command.Id);
+        var name = ArtistName.Create(command.Name);
+        var updatedArtist = name.Map(n => artist.Update(n));
+
+        await updatedArtist
+            .Succ(async a => { await _artistRepository.UpdateAsync(artist.Id); })
+            .Fail(e => e.AsTask());
+
+        return updatedArtist;
     }
 }
