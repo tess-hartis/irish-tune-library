dad3681bf90e3da0c874bebb1a7e9aafa9772bf7
tess-hartis <tess15hartis@gmail.com>
2022-02-17 14:22:53 -0600

    Refactor RemoveAlternateTitleCommandHandler to check for tune and tune title before removing

--- TL.Api/CQRS/TuneCQ/Commands/RemoveAlternateTitleCommandHandler.cs
+++ TL.Api/CQRS/TuneCQ/Commands/RemoveAlternateTitleCommandHandler.cs
@@ -1,5 +1,6 @@
 using LanguageExt;
 using LanguageExt.Common;
+using static LanguageExt.Prelude;
 using MediatR;
 using TL.Api.DTOs.TuneDTOS;
 using TL.Domain;
@@ -8,19 +9,19 @@ using TL.Repository;
 
 namespace TL.Api.CQRS.TuneCQ.Commands;
 
-public class RemoveAlternateTitleCommand : IRequest<Validation<Error, Tune>>
+public class RemoveAlternateTitleCommand : IRequest<Option<Tune>>
 {
     public int Id { get; set; }
-    public string AlternateTitle { get; }
+    public string AltTitleString { get; }
 
-    public RemoveAlternateTitleCommand(int id, string alternateTitle)
+    public RemoveAlternateTitleCommand(int id, string altTitleString)
     {
         Id = id;
-        AlternateTitle = alternateTitle;
+        AltTitleString = altTitleString;
     }
 }
 public class RemoveAlternateTitleCommandHandler : 
-    IRequestHandler<RemoveAlternateTitleCommand, Validation<Error, Tune>>
+    IRequestHandler<RemoveAlternateTitleCommand, Option<Tune>>
 {
     private readonly ITuneRepository _tuneRepository;
 
@@ -29,26 +30,52 @@ public class RemoveAlternateTitleCommandHandler :
         _tuneRepository = tuneRepository;
     }
 
-    public async Task<Validation<Error, Tune>> Handle
+    public async Task<Option<Tune>> Handle
         (RemoveAlternateTitleCommand command, CancellationToken cancellationToken)
     {
-        var tune = await _tuneRepository.FindAsync(command.Id);
-        var toDelete = TuneTitle.Create(command.AlternateTitle);
-
-        var updatedTune = (toDelete)
-            .Map(t => tune.RemoveAlternateTitle(t));
-
-        updatedTune
-            .Succ(async t =>
-            {
-                await _tuneRepository.UpdateAsync(tune.Id);
-            })
-            .Fail(e =>
-            {
-                return e.AsTask();
-            });
-
-        return updatedTune;
         
+        var tune = await _tuneRepository.FindAsync(command.Id);
+
+        var possibleTitle = tune
+            .Map(t => t.AlternateTitles
+                .First(x => x.Value == command.AltTitleString));
+
+        var result =
+            from t in tune
+            from p in possibleTitle
+            select t.RemoveAlternateTitle(p);
+
+        ignore(result.Map(async x => await _tuneRepository.UpdateAsync(x)));
+
+        return result;
+
+        // var tune = await _tuneRepository.FindAsync(command.Id);
+        //
+        // var unvalidated = command.AlternateTitle;
+        //
+        // var hmm = tune.Map(t =>
+        // {
+        //     if (t.AlternateTitles.Exists(x => x.Value == unvalidated))
+        //         
+        //         return t.RemoveAlternateTitle(unvalidated);
+        //     return t;
+        // });
+        // var updatedTune = tune.Map(t => t.RemoveAlternateTitle(unvalidated));
+        //
+        // tune.Map(t => hmm.Map(h =>
+        // {
+        //     if (t.AlternateTitles.Count > h.AlternateTitles.Count)
+        //         ignore(updatedTune
+        //             .Map(async x => await _tuneRepository.UpdateAsync(x)));
+        // }));
+        //
+        //
+        // return updatedTune;
+
+        // ignore(tune.Map(async x => await _tuneRepository.RemoveAlternateTitle(x, possibleTitleToDelete)));
+
+
+
+
     }
 }
