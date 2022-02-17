2090313b5084730666814428b81cbc2ad73fdb72
tess-hartis <tess15hartis@gmail.com>
2022-02-17 12:48:16 -0600

    Comment out file since all methods are actively used

--- TL.Repository/AlbumTrackService.cs
+++ TL.Repository/AlbumTrackService.cs
@@ -1,40 +1,40 @@
-using System.Diagnostics;
-using Microsoft.EntityFrameworkCore;
-using TL.Data;
-using TL.Domain;
-using TL.Domain.ValueObjects.TrackValueObjects;
-
-namespace TL.Repository;
-
-public interface IAlbumTrackService
-{
-    Task<Track> AddNewTrackToAlbum(Album album, Track track);
-    // Task RemoveTrackFromAlbum(int albumId, int trackId);
-    Task<IEnumerable<Track>> GetAlbumTracks(Album album);
-
-}
-public class AlbumTrackService : IAlbumTrackService
-{
-    private readonly TuneLibraryContext _context;
-    private readonly ITrackRepository _trackRepository;
-
-    public AlbumTrackService(TuneLibraryContext context, ITrackRepository trackRepository)
-    {
-        _context = context;
-        _trackRepository = trackRepository;
-    }
-    private async Task SaveChangesAsync()
-    {
-        await _context.SaveChangesAsync();
-    }
-
-    public async Task<Track> AddNewTrackToAlbum(Album album, Track track)
-    {
-        track.SetAlbumId(album.Id);
-        await _trackRepository.AddAsync(track);
-        await SaveChangesAsync();
-        return track;
-    }
+// using System.Diagnostics;
+// using Microsoft.EntityFrameworkCore;
+// using TL.Data;
+// using TL.Domain;
+// using TL.Domain.ValueObjects.TrackValueObjects;
+//
+// namespace TL.Repository;
+//
+// public interface IAlbumTrackService
+// {
+//     Task<Track> AddNewTrackToAlbum(Album album, Track track);
+//     // Task RemoveTrackFromAlbum(int albumId, int trackId);
+//     // Task<IEnumerable<Track>> GetAlbumTracks(Album album);
+//
+// }
+// public class AlbumTrackService : IAlbumTrackService
+// {
+//     private readonly TuneLibraryContext _context;
+//     private readonly ITrackRepository _trackRepository;
+//
+//     public AlbumTrackService(TuneLibraryContext context, ITrackRepository trackRepository)
+//     {
+//         _context = context;
+//         _trackRepository = trackRepository;
+//     }
+//     private async Task SaveChangesAsync()
+//     {
+//         await _context.SaveChangesAsync();
+//     }
+//
+//     public async Task<Track> AddNewTrackToAlbum(Album album, Track track)
+//     {
+//         track.SetAlbumId(album.Id);
+//         await _trackRepository.AddAsync(track);
+//         await SaveChangesAsync();
+//         return track;
+//     }
     
 
     // public async Task RemoveTrackFromAlbum(int albumId, int trackId)
@@ -45,12 +45,12 @@ public class AlbumTrackService : IAlbumTrackService
     //     await SaveChangesAsync();
     // }
 
-    public async Task<IEnumerable<Track>> GetAlbumTracks(Album album)
-    {
-        return await _trackRepository
-            .GetByWhere(x => x.AlbumId == album.Id)
-            .Include(x => x.Album)
-            .OrderBy(x => x.TrackNumber.Value)
-            .ToListAsync();
-    }
-}
+    // public async Task<IEnumerable<Track>> GetAlbumTracks(Album album)
+    // {
+    //     return await _trackRepository
+    //         .GetByWhere(x => x.AlbumId == album.Id)
+    //         .Include(x => x.Album)
+    //         .OrderBy(x => x.TrackNumber.Value)
+    //         .ToListAsync();
+    // }
+// }
