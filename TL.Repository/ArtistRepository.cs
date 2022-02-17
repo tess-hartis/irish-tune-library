6b6221850a1a41dbb44e5b08731b5c43d11e347f
tess-hartis <tess15hartis@gmail.com>
2022-02-16 23:00:46 -0600

    Add override FindAsync method to artist repository that returns Option<Artist>

--- TL.Repository/ArtistRepository.cs
+++ TL.Repository/ArtistRepository.cs
@@ -1,3 +1,5 @@
+using LanguageExt;
+using LanguageExt.SomeHelp;
 using Microsoft.EntityFrameworkCore;
 using TL.Data;
 using TL.Domain;
@@ -8,6 +10,7 @@ namespace TL.Repository;
 public interface IArtistRepository : IGenericRepository<Artist>
 {
     Task<IEnumerable<Artist>> GetAllArtists();
+    new Task<Option<Artist>> FindAsync(int id);
 }
 
 public class ArtistRepository : GenericRepository<Artist>, IArtistRepository
@@ -22,5 +25,13 @@ public class ArtistRepository : GenericRepository<Artist>, IArtistRepository
         return await Context.Artists.Include(a => a.Albums).ToListAsync();
     }
 
+    public override async Task<Option<Artist>> FindAsync(int id)
+    {
+        var result = await Context.Artists.FirstOrDefaultAsync(x => x.Id == id);
+        if (result == null)
+            return Option<Artist>.None;
+
+        return result.ToSome();
+    }
 
 }
