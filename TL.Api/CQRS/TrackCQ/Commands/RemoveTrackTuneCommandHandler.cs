24c6498a2f57a919bc683f57294f050bdc13f807
tess-hartis <tess15hartis@gmail.com>
2022-02-17 12:48:02 -0600

    Refactor command handler to return Option<Unit>

--- TL.Api/CQRS/TrackCQ/Commands/RemoveTrackTuneCommandHandler.cs
+++ TL.Api/CQRS/TrackCQ/Commands/RemoveTrackTuneCommandHandler.cs
@@ -1,34 +1,34 @@
-using MediatR;
-using TL.Domain;
-using TL.Repository;
+ using LanguageExt;
+ using static LanguageExt.Prelude;
+ using MediatR;
+ using TL.Domain;
+ using TL.Repository;
+ using Unit = LanguageExt.Unit;
 
-namespace TL.Api.CQRS.TrackCQ.Commands;
+ namespace TL.Api.CQRS.TrackCQ.Commands;
 
-public class RemoveTrackTuneCommand : IRequest<Track>
-{
-    public int TrackId { get; }
-    public int TuneId { get; }
+ public class RemoveTrackTuneCommand : IRequest<Option<Unit>>
+ {
+     public int TrackTuneId { get; set; }
 
-    public RemoveTrackTuneCommand(int trackId, int tuneId)
-    {
-        TrackId = trackId;
-        TuneId = tuneId;
-    }
-}
-public class RemoveTrackTuneCommandHandler : IRequestHandler<RemoveTrackTuneCommand, Track>
-{
-    private readonly ITuneTrackService _tuneTrackService;
+     public RemoveTrackTuneCommand(int trackTuneId)
+     {
+         TrackTuneId = trackTuneId;
+     }
+ }
+ public class RemoveTrackTuneCommandHandler : IRequestHandler<RemoveTrackTuneCommand, Option<Unit>>
+ {
+     private readonly ITrackTuneRepository _trackTuneRepository;
 
-    public RemoveTrackTuneCommandHandler(ITuneTrackService tuneTrackService)
-    {
-        _tuneTrackService = tuneTrackService;
-    }
+     public RemoveTrackTuneCommandHandler(ITrackTuneRepository trackTuneRepository)
+     {
+         _trackTuneRepository = trackTuneRepository;
+     }
 
-    public async Task<Track> Handle(RemoveTrackTuneCommand command, CancellationToken cancellationToken)
-    {
-        var track = await _tuneTrackService.RemoveTuneFromTrack(command.TrackId, command.TuneId);
-        return track;
-        
-        //update DTO to also show tunes on track
-    }
-}
+     public async Task<Option<Unit>> Handle(RemoveTrackTuneCommand command, CancellationToken cancellationToken)
+     {
+         var trackTune = await _trackTuneRepository.FindAsync(command.TrackTuneId);
+         ignore(trackTune.Map(async t => await _trackTuneRepository.DeleteAsync(t)));
+         return trackTune.Map(t => unit);
+     }
+ }
