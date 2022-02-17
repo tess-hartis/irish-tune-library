b67e8c7308e9189f4de3057e130a3bdbc6363d4b
tess-hartis <tess15hartis@gmail.com>
2022-02-17 12:56:47 -0600

    Refactor command handler to return Option<Validation<Erro>>

--- TL.Api/CQRS/TrackCQ/Commands/AddTrackTuneCommandHandler.cs
+++ TL.Api/CQRS/TrackCQ/Commands/AddTrackTuneCommandHandler.cs
@@ -1,5 +1,7 @@
 using LanguageExt;
 using LanguageExt.Common;
+using LanguageExt.SomeHelp;
+using static LanguageExt.Prelude;
 using MediatR;
 using TL.Api.DTOs.TrackDTOs;
 using TL.Domain;
@@ -8,7 +10,7 @@ using TL.Repository;
 
 namespace TL.Api.CQRS.TrackCQ.Commands;
 
-public class AddTrackTuneCommand : IRequest<Validation<Error, TrackTune>>
+public class AddTrackTuneCommand : IRequest<Option<Validation<Error, TrackTune>>>
 {
     public int TrackId { get; set; }
     public int TuneId { get; set; }
@@ -21,28 +23,63 @@ public class AddTrackTuneCommand : IRequest<Validation<Error, TrackTune>>
         Order = order;
     }
 }
-public class AddTrackTuneCommandHandler : IRequestHandler<AddTrackTuneCommand, Validation<Error, TrackTune>>
+public class AddTrackTuneCommandHandler : IRequestHandler<AddTrackTuneCommand, Option<Validation<Error, TrackTune>>>
 {
-    private readonly ITuneTrackService _tuneTrackService;
+    private readonly ITrackRepository _trackRepository;
+    private readonly ITuneRepository _tuneRepository;
+    private readonly ITrackTuneRepository _trackTuneRepository;
 
-    public AddTrackTuneCommandHandler(ITuneTrackService tuneTrackService)
+    public AddTrackTuneCommandHandler(ITrackRepository trackRepository, ITuneRepository tuneRepository, ITrackTuneRepository trackTuneRepository)
     {
-        _tuneTrackService = tuneTrackService;
+        _trackRepository = trackRepository;
+        _tuneRepository = tuneRepository;
+        _trackTuneRepository = trackTuneRepository;
     }
 
-    public async Task<Validation<Error, TrackTune>> Handle(AddTrackTuneCommand command, CancellationToken cancellationToken)
+    public async Task<Option<Validation<Error, TrackTune>>> Handle(AddTrackTuneCommand command,
+        CancellationToken cancellationToken)
     {
-        var order = TrackTuneOrder.Create(command.Order);
-        var trackTune = order
-            .Map(o => TrackTune.Create(command.TrackId, command.TuneId, o));
+        var track = await _trackRepository.FindAsync(command.TrackId);
+        var tune = await _tuneRepository.FindAsync(command.TuneId);
+        var order = Some(TrackTuneOrder.Create(command.Order));
 
-        await trackTune
-            .Succ(async t =>
-            {
-                await _tuneTrackService.AddExistingTuneToTrack(command.TrackId, command.TuneId, t);
-            })
-            .Fail(e => e.AsTask());
+        var result =
+            from tr in track
+            from tu in tune
+            from o in order
+            select o.Map(x => TrackTune.Create(tr, tu, x));
 
-        return trackTune;
+        ignore(result
+            .Map(x =>
+                x.Map(async y => await _trackTuneRepository.AddAsync(y))));
+
+        return result;
+
+        // var succTrack = Success<Error, Option<Track>>(track);
+        // var succTune = Success<Error, Option<Tune>>(tune);
+
+        // var newTrackTune = (track, tune).Apply((tr, tu) => TrackTune.Create(tr, tu, or));
+        //
+        // var newtttt = track
+        //     .Bind(x => tune.Map(y => order
+        //         .Map(o => TrackTune.Create(x, y, o))));
+        //
+        // ignore(newtttt.Map(t => t
+        //     .Map(async x => await _trackTuneRepository.AddAsync(x))));
+        //
+        // return newtttt;
+
+        // var querySyntax = from tr in track from tu in tune from o in order select TrackTune.Create(tr, tu, o);
+        //
+        // var sdjfs = from or in order select or.Value;
+        //
+        // var optOne = 1.ToSome().ToOption();
+        //
+        // var result = optOne.Bind(x => x > 5 ? Some(x + 1) : Option<int>.None);
+
+
     }
 }
+
+
+        // M(a -> b) -> Ma -> Mb
