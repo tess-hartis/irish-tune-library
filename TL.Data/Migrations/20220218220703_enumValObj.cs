4ef034a61c17ed37217bed391a39d42232c48102
tess-hartis <tess15hartis@gmail.com>
2022-02-18 16:39:33 -0600

    Migration files

---
+++ TL.Data/Migrations/20220218220703_enumValObj.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TL.Data.Migrations
+{
+    public partial class enumValObj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TuneType",
+                table: "Tunes",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "TuneKey",
+                table: "Tunes",
+                newName: "Key");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Tunes",
+                newName: "TuneType");
+
+            migrationBuilder.RenameColumn(
+                name: "Key",
+                table: "Tunes",
+                newName: "TuneKey");
+        }
+    }
+}
