63b74398daca543270a7bd7bf7a3be62b19015ef
tess-hartis <tess15hartis@gmail.com>
2022-03-02 20:33:38 -0600

    Remove not implemented interface member

--- TL.Repository/TrackTuneUnitOfWork.cs
+++ TL.Repository/TrackTuneUnitOfWork.cs
@@ -7,7 +7,6 @@ public interface ITrackTuneUnitOfWork
     Task Save();
     ITrackRepository TrackRepo { get; }
     ITuneRepository TuneRepo { get; }
-    ITrackTuneRepository TrackTuneRepo { get; }
 }
 
 public class TrackTuneUnitOfWork : ITrackTuneUnitOfWork
@@ -15,7 +14,6 @@ public class TrackTuneUnitOfWork : ITrackTuneUnitOfWork
     private readonly TuneLibraryContext _context;
     private ITrackRepository _trackRepository;
     private ITuneRepository _tuneRepository;
-    private ITrackTuneRepository _trackTuneRepository;
 
     public TrackTuneUnitOfWork(TuneLibraryContext context)
     {
@@ -37,15 +35,7 @@ public class TrackTuneUnitOfWork : ITrackTuneUnitOfWork
             return _tuneRepository = new TuneRepository(_context);
         }
     }
-
-    public ITrackTuneRepository TrackTuneRepo
-    {
-        get
-        {
-            return _trackTuneRepository = new TrackTuneRepository(_context);
-        }
-    }
-
+    
     public async Task Save()
     {
         await _context.SaveChangesAsync();
