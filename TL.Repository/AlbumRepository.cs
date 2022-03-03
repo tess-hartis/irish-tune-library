bb966e30a1a8cd99badf560645392c908d971435
tess-hartis <tess15hartis@gmail.com>
2022-03-02 20:22:13 -0600

    Clean up, remove comments

--- TL.Repository/AlbumRepository.cs
+++ TL.Repository/AlbumRepository.cs
@@ -3,14 +3,12 @@ using LanguageExt.SomeHelp;
 using Microsoft.EntityFrameworkCore;
 using TL.Data;
 using TL.Domain;
-using TL.Domain.ValueObjects.AlbumValueObjects;
 
 namespace TL.Repository;
 
 public interface IAlbumRepository : IGenericRepository<Album>
 {
     new Task<Option<Album>> FindAsync(int id);
-    // new Task DeleteAsync(int id);
     Task<IEnumerable<Album>> GetAll();
 }
 
@@ -34,20 +32,6 @@ public class AlbumRepository : GenericRepository<Album>, IAlbumRepository
         return album.ToSome();
     }
     
-    // public override async Task DeleteAsync(int id)
-    // {
-    //     var album = await Context.Albums
-    //         .Include(x => x.TrackListing)
-    //         .FirstOrDefaultAsync(x => x.Id == id);
-    //     
-    //     if (album == null)
-    //         throw new EntityNotFoundException($"No album with ID of '{id}' was found");
-    //     
-    //     Context.Remove(album);
-    //     await SaveAsync();
-    // }
-    
-
     public async Task<IEnumerable<Album>> GetAll()
     {
         return await Context.Albums
