1e5f8eedb62044d61e779cda704f5c8b8f9ed3e0
tess-hartis <tess15hartis@gmail.com>
2022-03-02 20:22:27 -0600

    Implement unit of work

--- TL.Api/CQRS/TrackCQ/Commands/AddTrackTuneCommandHandler.cs
+++ TL.Api/CQRS/TrackCQ/Commands/AddTrackTuneCommandHandler.cs
@@ -1,10 +1,7 @@
 using LanguageExt;
 using LanguageExt.Common;
-using LanguageExt.SomeHelp;
 using static LanguageExt.Prelude;
 using MediatR;
-using TL.Api.DTOs.TrackDTOs;
-using TL.Data;
 using TL.Domain;
 using TL.Domain.ValueObjects.TrackTuneValueObjects;
 using TL.Repository;
@@ -26,45 +23,18 @@ public class AddTrackTuneCommand : IRequest<Option<Validation<Error, bool>>>
 }
 public class AddTrackTuneCommandHandler : IRequestHandler<AddTrackTuneCommand, Option<Validation<Error, bool>>>
 {
-    private readonly TuneLibraryContext _context;
-    private ITrackRepository _trackRepository;
-    private ITuneRepository _tuneRepository;
-    private ITrackTuneRepository _trackTuneRepository;
+    private readonly ITrackTuneUnitOfWork _unitOfWork;
 
-    public AddTrackTuneCommandHandler(TuneLibraryContext context)
+    public AddTrackTuneCommandHandler(ITrackTuneUnitOfWork unitOfWork)
     {
-        _context = context;
+        _unitOfWork = unitOfWork;
     }
-
-    private ITrackRepository TrackRepo
-    {
-        get
-        {
-            return _trackRepository = new TrackRepository(_context);
-        }
-    }
-
-    private ITuneRepository TuneRepo
-    {
-        get
-        {
-            return _tuneRepository = new TuneRepository(_context);
-        }
-    }
-
-    private ITrackTuneRepository TrackTuneRepo
-    {
-        get
-        {
-            return _trackTuneRepository = new TrackTuneRepository(_context);
-        }
-    }
-
+    
     public async Task<Option<Validation<Error, bool>>> Handle(AddTrackTuneCommand command,
         CancellationToken cancellationToken)
     {
-        var track = await TrackRepo.FindAsync(command.TrackId);
-        var tune = await TuneRepo.FindAsync(command.TuneId);
+        var track = await _unitOfWork.TrackRepo.FindAsync(command.TrackId);
+        var tune = await _unitOfWork.TuneRepo.FindAsync(command.TuneId);
         var order = Some(TrackTuneOrder.Create(command.Order));
 
         var result =
@@ -74,35 +44,8 @@ public class AddTrackTuneCommandHandler : IRequestHandler<AddTrackTuneCommand, O
             select o.Map(TrackTune.Create)
                 .Map(y => tr.AddTrackTune(y, tu));
 
-        ignore(result.MapT(async y => await _context.SaveChangesAsync()));
+        ignore(result.MapT(async y => await _unitOfWork.Save()));
 
         return result;
-
-        // var succTrack = Success<Error, Option<Track>>(track);
-        // var succTune = Success<Error, Option<Tune>>(tune);
-
-        // var newTrackTune = (track, tune).Apply((tr, tu) => TrackTune.Create(tr, tu, or));
-        //
-        // var newtttt = track
-        //     .Bind(x => tune.Map(y => order
-        //         .Map(o => TrackTune.Create(x, y, o))));
-        //
-        // ignore(newtttt.Map(t => t
-        //     .Map(async x => await _trackTuneRepository.AddAsync(x))));
-        //
-        // return newtttt;
-
-        // var querySyntax = from tr in track from tu in tune from o in order select TrackTune.Create(tr, tu, o);
-        //
-        // var sdjfs = from or in order select or.Value;
-        //
-        // var optOne = 1.ToSome().ToOption();
-        //
-        // var result = optOne.Bind(x => x > 5 ? Some(x + 1) : Option<int>.None);
-
-
     }
 }
-
-
-        // M(a -> b) -> Ma -> Mb
